bd764a1b0d63e2806cc51dddae90ab7d5e12e350
alexc2029 <alex2akec@gmail.com>
2025-12-23 12:20:49 +0200

    feat: render ships on gameboards

--- src/js/dom.js
+++ src/js/dom.js
@@ -16,4 +16,25 @@ export const DisplayController = (() => {
 		gameboardsContainer.appendChild(gameboard1);
 		gameboardsContainer.appendChild(gameboard2);
 	})();
+	const updateGameboard = (gameboard, squares) => {
+		for (let i = 1; i <= 10; i++) {
+			for (let j = 1; j <= 10; j++) {
+				if (gameboard.at([i, j]).ship)
+					squares[(j - 1) * 10 + (i - 1)].classList.add("ship");
+			}
+		}
+	};
+	const updateGameboards = (gameboard1, gameboard2) => {
+		const squares1 = document.querySelectorAll(
+			".gameboards-container > .gameboard:nth-child(1) > div",
+		);
+		const squares2 = document.querySelectorAll(
+			".gameboards-container > .gameboard:nth-child(2) > div",
+		);
+		console.log(squares1);
+		console.log(squares2);
+		updateGameboard(gameboard1, squares1);
+		updateGameboard(gameboard2, squares2);
+	};
+	return { updateGameboards };
 })();
