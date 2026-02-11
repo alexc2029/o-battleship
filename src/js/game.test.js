ae629145d7a27d8aa71c3d0334eb4e21814c1d4b
alexc2029 <alex2akec@gmail.com>
2026-02-11 15:07:14 +0200

    test: add processAttack tests

--- src/js/game.test.js
+++ src/js/game.test.js
@@ -1,20 +1,41 @@
-import { Gameboard } from "./gameboard";
 import { DisplayController } from "./dom";
 import { processAttack } from "./game";
 
-jest.mock("./gameboard");
-jest.mock("./dom");
+jest.mock("./dom", () => {
+	return {
+		DisplayController: {
+			renderSquareHit: jest.fn(),
+			renderSquareMiss: jest.fn(),
+		},
+	};
+});
 
 describe("game tests", () => {
 	describe("processAttack tests", () => {
+		let mockGameboard,
+			mockSquare,
+			mockDomSquare = null,
+			mockCoords = [0, 0];
 		beforeEach(() => {
-			const gameboard = new Gameboard();
-			const testCoords = [5, 5];
-			const square = null;
-			processAttack(gameboard, testCoords, square);
+			mockSquare = { isHit: jest.fn() };
+			mockGameboard = {
+				receiveAttack: jest.fn(),
+				at: jest.fn().mockReturnValue(mockSquare),
+			};
 		});
 		test("processAttack calls Gameboard.receiveAttack", () => {
-			expect(receiveAttack).toHaveBeenCalled();
+			processAttack(mockGameboard, mockCoords, mockDomSquare);
+			expect(mockGameboard.receiveAttack).toHaveBeenCalled();
+		});
+		test("processAttack calls renderSquareHit on hit", () => {
+			mockSquare.isHit.mockReturnValue(true);
+			processAttack(mockGameboard, mockCoords, mockDomSquare);
+			expect(DisplayController.renderSquareHit).toHaveBeenCalled();
+		});
+		test("processAttack calls renderSquareMiss on miss", () => {
+			mockSquare.isHit.mockReturnValue(false);
+			processAttack(mockGameboard, mockCoords, mockDomSquare);
+			expect(DisplayController.renderSquareMiss).toHaveBeenCalled();
 		});
 	});
 });
