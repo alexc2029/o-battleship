a92ffc0e03c2ff43b41079bbd7c7c5f3be288e5b
alexc2029 <alex2akec@gmail.com>
2026-03-03 16:21:07 +0200

    fix: show gameboards before game start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import "./styles.css";
 import { GameController } from "./js/game";
 import { setGameReset } from "./js/events";
+import DisplayController from "./js/dom.js";
+
 const playButton = document.querySelector(".play-button");
 let firstPress = true;
+DisplayController.init();
+DisplayController.renderGameboards();
 playButton.addEventListener("click", () => {
 	if (firstPress) {
 		firstPress = false;
