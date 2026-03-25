e7e7aba0e3bd8852ae50c0b1d1cde0892b0d4a02
alexc2029 <alex2akec@gmail.com>
2026-03-25 12:16:29 +0200

    fix(gameboard tests): silent syntax errors and replace fragile shared test state

--- src/js/gameboard.test.js
+++ src/js/gameboard.test.js
@@ -2,7 +2,7 @@ import { Gameboard } from "./gameboard";
 
 describe("gameboard tests", () => {
 	let gameboard;
-	beforeAll(() => {
+	beforeEach(() => {
 		gameboard = new Gameboard();
 		gameboard.place([5, 5], 5);
 	});
@@ -21,26 +21,26 @@ describe("gameboard tests", () => {
 	});
 	test("square receives attack", () => {
 		gameboard.receiveAttack([5, 5]);
-		expect(gameboard.at([5, 5]).isHit()).toBeTruthy;
+		expect(gameboard.at([5, 5]).isHit()).toBeTruthy();
 	});
 	test("empty square records miss", () => {
 		gameboard.receiveAttack([4, 5]);
-		expect(gameboard.at([4, 5]).isMissed()).toBeTruthy;
+		expect(gameboard.at([4, 5]).isMissed()).toBeTruthy();
 	});
 	test("reports all sunk after sole ship is attacked on every square", () => {
 		for (let i = 5; i <= 9; i++) {
 			gameboard.receiveAttack([i, 5]);
 		}
-		expect(gameboard.allSunk()).toBeTruthy;
+		expect(gameboard.allSunk()).toBeTruthy();
 	});
 	test("does not report all sunk when sole ship is undamaged", () => {
-		expect(gameboard.allSunk()).toBeFalsy;
+		expect(gameboard.allSunk()).toBeFalsy();
 	});
 	test("does not report all sunk when sole ship is partially damaged", () => {
 		for (let i = 5; i <= 8; i++) {
 			gameboard.receiveAttack([i, 5]);
 		}
-		expect(gameboard.allSunk()).toBeFalsy;
+		expect(gameboard.allSunk()).toBeFalsy();
 	});
 	test("random coords are between 0 and 10", () => {
 		expect(gameboard.getRandomCoords()[0]).toBeGreaterThanOrEqual(0);
@@ -50,8 +50,8 @@ describe("gameboard tests", () => {
 	});
 	test("already shot is true for a hit square", () => {
 		gameboard.receiveAttack([5, 5]);
-		expect(gameboard.alreadyShot([5, 5])).toBeTruthy;
-		expect(gameboard.alreadyShot([4, 5])).toBeFalsy;
+		expect(gameboard.alreadyShot([5, 5])).toBeTruthy();
+		expect(gameboard.alreadyShot([4, 5])).toBeFalsy();
 	});
 	describe("areCoordsValid tests", () => {
 		let gameboard;
