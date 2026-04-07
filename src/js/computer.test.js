c977222b2ef22600c842d1bf2e584788a6567077
alexc2029 <alex2akec@gmail.com>
2026-04-07 09:48:25 +0300

    refactor: group computer horizontal attack tests into describe block

--- src/js/computer.test.js
+++ src/js/computer.test.js
@@ -13,41 +13,52 @@ describe("Computer tests", () => {
 		afterEach(() => {
 			jest.restoreAllMocks();
 		});
-		test("eventually attacks second square for a 2 length ship", () => {
-			playerGameboard.place([3, 3], 2);
-			stubRandomAttack.mockReturnValue([3, 3]);
-			const attacks = [];
-			for (let i = 0; i <= 4; i++) {
-				attacks.push(
-					computer.decideAttack(playerGameboard, stubRandomAttack),
-				);
-			}
-			expect(attacks).toContainEqual([4, 3]);
-		});
-		test("eventually attacks all squares for the middle of a 3 length ship", () => {
-			playerGameboard.place([3, 3], 3);
-			stubRandomAttack.mockReturnValue([4, 3]);
-			const attacks = [];
-			for (let i = 0; i <= 4; i++) {
-				attacks.push(
-					computer.decideAttack(playerGameboard, stubRandomAttack),
-				);
-			}
-			expect(attacks).toContainEqual([4, 3]);
-			expect(attacks).toContainEqual([5, 3]);
-		});
-		test("eventually attacks all squares for the middle of a 4 length ship", () => {
-			playerGameboard.place([3, 3], 4);
-			stubRandomAttack.mockReturnValue([4, 3]);
-			const attacks = [];
-			for (let i = 0; i <= 8; i++) {
-				attacks.push(
-					computer.decideAttack(playerGameboard, stubRandomAttack),
-				);
-			}
-			expect(attacks).toContainEqual([4, 3]);
-			expect(attacks).toContainEqual([5, 3]);
-			expect(attacks).toContainEqual([6, 3]);
+		describe("horizontal", () => {
+			test("eventually attacks second square for a 2 length ship", () => {
+				playerGameboard.place([3, 3], 2);
+				stubRandomAttack.mockReturnValue([3, 3]);
+				const attacks = [];
+				for (let i = 0; i <= 4; i++) {
+					attacks.push(
+						computer.decideAttack(
+							playerGameboard,
+							stubRandomAttack,
+						),
+					);
+				}
+				expect(attacks).toContainEqual([4, 3]);
+			});
+			test("eventually attacks all squares for the middle of a 3 length ship", () => {
+				playerGameboard.place([3, 3], 3);
+				stubRandomAttack.mockReturnValue([4, 3]);
+				const attacks = [];
+				for (let i = 0; i <= 4; i++) {
+					attacks.push(
+						computer.decideAttack(
+							playerGameboard,
+							stubRandomAttack,
+						),
+					);
+				}
+				expect(attacks).toContainEqual([4, 3]);
+				expect(attacks).toContainEqual([5, 3]);
+			});
+			test("eventually attacks all squares for the middle of a 4 length ship", () => {
+				playerGameboard.place([3, 3], 4);
+				stubRandomAttack.mockReturnValue([4, 3]);
+				const attacks = [];
+				for (let i = 0; i <= 8; i++) {
+					attacks.push(
+						computer.decideAttack(
+							playerGameboard,
+							stubRandomAttack,
+						),
+					);
+				}
+				expect(attacks).toContainEqual([4, 3]);
+				expect(attacks).toContainEqual([5, 3]);
+				expect(attacks).toContainEqual([6, 3]);
+			});
 		});
 	});
 	describe("isShipVertical tests", () => {
